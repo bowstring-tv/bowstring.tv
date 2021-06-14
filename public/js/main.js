bcc93c4b89f887a01cdd24b7a470440756a5e751
DesarrolloDMIX <dev@digitalmix.ar>
2021-06-14 16:19:17 -0300

    Corrections in footer

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,12 @@ const navButtonOpen = document.querySelector('.nav-open button')
 const navButtonClose = document.querySelector('.nav-close button')
 const nav = document.querySelector('.main-nav')
 
-navButtonOpen.addEventListener('click', (evt) => {
-    nav.classList.add('open')
-})
+if (navButtonOpen) {
+    navButtonOpen.addEventListener('click', (evt) => {
+        nav.classList.add('open')
+    })
 
-navButtonClose.addEventListener('click', (evt) => {
-    nav.classList.remove('open')
-})
+    navButtonClose.addEventListener('click', (evt) => {
+        nav.classList.remove('open')
+    })
+}
